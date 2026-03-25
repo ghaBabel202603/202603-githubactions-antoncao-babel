c047611f04b6292f506f4c2d8a1079b80c26d2f4
antoncao <anton.cao@babelgroup.com>
2026-03-25 15:30:26 +0100

    Ejercicio 17.4

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,7 +1,44 @@
 const core = require('@actions/core');
+const exec = require('@actions/exec');
 
 async function run() {
-    core.info('I am a custom JS action');
+    const baseBranch = core.getInput('base-branch');
+    const targetBranch = core.getInput('target-branch');
+    const workingDirectory = core.getInput('working-directory');
+    const ghToken = core.getInput('gh-token');
+    const debug = core.getBooleanInput('debug');
+
+    const branchRegex = /^[a-zA-Z0-9_.\-\/]+$/;
+    const dirRegex = /^[a-zA-Z0-9_\-\/]+$/;
+
+    if (!branchRegex.test(baseBranch)) {
+        core.setFailed('The base branch name is invalid.');
+        return;
+    }
+    if (!branchRegex.test(targetBranch)) {
+        core.setFailed('The target branch name is invalid.');
+        return;
+    }
+    if (!dirRegex.test(workingDirectory)) {
+        core.setFailed('The working directory path is invalid.');
+        return;
+    }
+
+    core.info(`Base branch: ${baseBranch}`);
+    core.info(`Target branch: ${targetBranch}`);
+    core.info(`Working directory: ${workingDirectory}`);
+
+    await exec.exec('npm', ['update'], { cwd: workingDirectory });
+
+    const gitStatusOutput = await exec.getExecOutput(
+        'git', ['status', '-s', 'package*.json'], { cwd: workingDirectory }
+    );
+
+    if (gitStatusOutput.stdout) {
+        core.info('There are updates available.');
+    } else {
+        core.info('There are no updates at this point in time.');
+    }
 }
 
 run();
